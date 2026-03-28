5fa5af90ca97dd1d7cdeab92e9dfac0a72a60405
Ben Toon <bentoon00@gmail.com>
2026-03-28 15:50:05 +0000

    Moved new design into main webpage repo

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.RgWXzvPp.js","_app/immutable/chunks/C9kA06Cv.js","_app/immutable/chunks/BhboSial.js","_app/immutable/chunks/Cuo_uVMx.js","_app/immutable/chunks/DmpaQNZE.js","_app/immutable/chunks/eu7lJzMi.js","_app/immutable/chunks/D8p9mwtT.js","_app/immutable/chunks/C6lQ870R.js"];
-export const stylesheets = [];
-export const fonts = [];
--- README.md
+++
@@ -1,4 +0,0 @@
-# Ben's Portfolio
-This is a simple site, based on SvelteKit and the 7.css library. I mainly created this to develop my web development skills. 
-
-- Hosted via Cloudflare Pages at [btn00.com](https://btn00.com)
