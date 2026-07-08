25b256c2c6e51968505ea8145947fd7a5ebb7714
Ben Toon <bentoon00@gmail.com>
2026-07-08 21:29:51 +0100

    Separation of index.html into Astro components. Begin modular layout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "Workbench",
+        cssVariable: "--font-workbench",
+        options: {
+            variants: [{
+                src: ['./src/fonts/Workbench.ttf'],
+                weight: 'normal',
+                style: 'normal'
+            }]
+        }
+    }]
+});
